6c1c21718c270d5777fb9cddb00165558e285737
DongVo333 <vandongvomd@gmail.com>
2021-07-09 22:33:58 +0700

    Import210709

--- gcodeproj/gcodedb/views.py
+++ gcodeproj/gcodedb/views.py
@@ -4,9 +4,9 @@ from django.views.generic import ListView, DetailView,CreateView,FormView
 from django.http import HttpResponse,HttpResponseRedirect
 from django.forms import ModelForm, formsets, inlineformset_factory,modelformset_factory,formset_factory
 from django.views.generic import CreateView, ListView, UpdateView
-from .models import Contract, G1code, G2code, GDV, Gcode,Inquiry,Client, Kho,Supplier,Lydowin,Lydoout
+from .models import Contract, DanhgiaNSX, Danhgiacode, G1code, G2code, GDV, Gcode, Giaohang,Inquiry,Client, Kho, POdetail, Phat, Sales,Supplier,Lydowin,Lydoout, Tienve
 from django.db import transaction,IntegrityError
-from .forms import GcodeForm, KhoForm, OfferForm, SearchQueryForm, ClientForm, InquiryForm,GDVForm,SupplierForm,ContractForm,LydooutForm,LydowinForm,OfferResultForm
+from .forms import DanhgiaNSXForm, DanhgiacodeForm, GcodeForm, GiaohangForm, HDBForm, KhoForm, OfferForm, POForm, PhatForm, SalesForm, SearchQueryForm, ClientForm, InquiryForm,GDVForm,SupplierForm,ContractForm,LydooutForm,LydowinForm,OfferResultForm, TienveForm
 from django.contrib import messages
 from django.urls import reverse_lazy
 from tablib import Dataset
@@ -374,35 +374,33 @@ def kho_form(request, id=None):
         if id == None:
             form = KhoForm()
         else:
-            g2codes = G2code.objects.filter(g2code = id)
-            kho = Kho.objects.get(pk=g2codes.g1code.id)
+            kho = Kho.objects.get(pk=id)
             form = KhoForm(instance=kho)
         return render(request, "gcodedb/kho_form.html", {'form': form})
     else:
         if id == None:
             form = KhoForm(request.POST)
         else:
-            g2codes = G2code.objects.filter(g2code = id)
-            kho = Kho.objects.get(pk=g2codes.g1code.id)
+            kho = Kho.objects.get(pk=id)
             form = KhoForm(request.POST,instance= kho)
         if form.is_valid():
-            form.save()
+            kho_ = form.save(commit=False)
+            kho_.dateupdate = date.today()
+            kho_.save()
         return redirect('/kho/')
 
 def kho_delete(request,id):
-    g2codes = G2code.objects.filter(g2code = id)
-    kho = Kho.objects.get(pk=g2codes.g1code.id)
+    kho = Kho.objects.get(pk=id)
     kho.delete()
     return redirect('/kho/')
 
 def SearchHDB(request):
     g1code_list = G1code.objects.filter(resultinq='Win')
     g1code_filter = G1codeFilter(request.GET, queryset=g1code_list)
-    g1code_temp = G1code.objects.filter(resultinq='Win',inquirycode=request.GET or None)
     for item in g1code_filter.qs:
         g1code_ = G1code.objects.get(pk = item.id)
         if G2code.objects.filter(g1code=g1code_).count()<=0:
-            g2code = G2code.objects.create(g1code=g1code_, dateupdate = date.today())
+            G2code.objects.create(g1code=g1code_,dongiachaohdb = g1code_.dongiachaoinq, dateupdate = date.today())
     g2code_list = G2code.objects.all()
     return render(request, 'gcodedb/hdb.html', {'g2code_list': g2code_list, 'filter':g1code_filter})
 
@@ -415,20 +413,27 @@ def offer_form(request, id=None):
         if id == None:
             form = OfferForm()
         else:
-            g1codes = G1code.objects.filter(pk = id)
+            g1codes = G1code.objects.get(pk = id)
             form = OfferForm(instance=g1codes)
         return render(request, "gcodedb/offer_form.html", {'form': form})
     else:
         if id == None:
             form = OfferForm(request.POST)
         else:
-            g1codes = G1code.objects.filter(pk = id)
+            g1codes = G1code.objects.get(pk = id)
             form = OfferForm(request.POST,instance= g1codes)
         if form.is_valid():
             g1code = form.save(commit=False)
             g1code.dateupdate = date.today()
-            g1code.g1code = g1code.gcode +"-"+g1code.inquiry.inquirycode
+            g1code.g1code = g1code.gcode.ma +"-"+g1code.inquiry.inquirycode
             g1code.save()
+            lydowins = form.cleaned_data.get('lydowin')
+            for lydoitem in lydowins:
+                g1code.lydowin.add(lydoitem)
+            lydoouts = form.cleaned_data.get('lydoout')
+            for lydoitem in lydoouts:
+                g1code.lydoout.add(lydoitem)
+            
         else:
             print(form.add_error)
         return redirect('/offer/')
@@ -437,3 +442,256 @@ def offer_delete(request,id):
     g1codes = G1code.objects.filter(pk = id)
     g1codes.delete()
     return redirect('/offer/')
+
+def sales_list(request):
+	sales_list = Sales.objects.all()
+	return render(request, 'gcodedb/sales_list.html', {'sales_list':sales_list})
+
+def sales_form(request, id=None):
+    if request.method == "GET":
+        if id == None:
+            form = SalesForm()
+        else:
+            varsales = Sales.objects.get(pk=id)
+            form = SalesForm(instance=varsales)
+        return render(request, "gcodedb/sales_form.html", {'form': form})
+    else:
+        if id == None:
+            form = SalesForm(request.POST)
+        else:
+            varsales = Sales.objects.get(pk=id)
+            form = SalesForm(request.POST,instance= varsales)
+        if form.is_valid():
+            form.save()
+        return redirect('/sales/')
+
+def sales_delete(request,id):
+    varsales = Sales.objects.get(pk=id)
+    varsales.delete()
+    return redirect('/sales/')
+
+def hdb_list(request):
+    hdb_list = G2code.objects.none
+    if request.method == "POST":
+        g1code_list = G1code.objects.filter(resultinq='Win',inquiry__inquirycode__icontains=request.POST.get("inquirysearch"))
+        if g1code_list.count() > 0: 
+            for item in g1code_list:
+                g1code_ = G1code.objects.get(pk = item.id)
+                if G2code.objects.filter(g1code=g1code_).count()<=0:
+                    G2code.objects.create(g1code=g1code_,dongiachaohdb = g1code_.dongiachaoinq, dateupdate = date.today())
+            return render(request, 'gcodedb/hdb_list.html', {'hdb_list': hdb_list})
+    return render(request, 'gcodedb/hdb_list.html', {'hdb_list': hdb_list})
+
+def hdb_form(request, id=None):
+    if request.method == "GET":
+        if id == None:
+            form = HDBForm()
+        else:
+            g2codes = G2code.objects.get(pk = id)
+            form = HDBForm(instance=g2codes)
+        return render(request, "gcodedb/hdb_form.html", {'form': form})
+    else:
+        if id == None:
+            form = HDBForm(request.POST)
+        else:
+            g2codes = G2code.objects.get(pk = id)
+            form = HDBForm(request.POST,instance= g2codes)
+        if form.is_valid():
+            g2codes = form.save(commit=False)
+            g2codes.dateupdate = date.today()
+            g2codes.g2code = g2codes.g1code.gcode.ma +"-"+ g2codes.contract.contractcode
+            g2codes.save()
+        else:
+            print(form.add_error)
+        return redirect('/hdb/')
+
+def hdb_delete(request,id):
+    g2codes = G2code.objects.filter(pk = id)
+    g2codes.delete()
+    return redirect('/hdb/')
+
+def po_list(request):
+	po_list = POdetail.objects.all()
+	return render(request, 'gcodedb/po_list.html', {'po_list':po_list})
+
+def po_form(request, id=None):
+    if request.method == "GET":
+        if id == None:
+            form = POForm()
+        else:
+            g2codes = POdetail.objects.get(pk = id)
+            form = POForm(instance=g2codes)
+        return render(request, "gcodedb/po_form.html", {'form': form})
+    else:
+        if id == None:
+            form = POForm(request.POST)
+        else:
+            g2codes = POdetail.objects.get(pk = id)
+            form = POForm(request.POST,instance= g2codes)
+        if form.is_valid():
+            g2codes = form.save(commit=False)
+            g2codes.dateupdate = date.today()
+            g2codes.save()
+        else:
+            print(form.add_error)
+        return redirect('/po/')
+
+def po_delete(request,id):
+    g2codes = POdetail.objects.filter(pk = id)
+    g2codes.delete()
+    return redirect('/po/')
+
+def giaohang_list(request):
+	giaohang_list = Giaohang.objects.all()
+	return render(request, 'gcodedb/giaohang_list.html', {'giaohang_list':giaohang_list})
+
+def giaohang_form(request, id=None):
+    if request.method == "GET":
+        if id == None:
+            form = GiaohangForm()
+        else:
+            g2codes = Giaohang.objects.get(pk = id)
+            form = GiaohangForm(instance=g2codes)
+        return render(request, "gcodedb/giaohang_form.html", {'form': form})
+    else:
+        if id == None:
+            form = GiaohangForm(request.POST)
+        else:
+            g2codes = Giaohang.objects.get(pk = id)
+            form = GiaohangForm(request.POST,instance= g2codes)
+        if form.is_valid():
+            g2codes = form.save(commit=False)
+            g2codes.dateupdate = date.today()
+            g2codes.save()
+        else:
+            print(form.add_error)
+        return redirect('/giaohang/')
+
+def giaohang_delete(request,id):
+    g2codes = Giaohang.objects.filter(pk = id)
+    g2codes.delete()
+    return redirect('/giaohang/')
+
+def tienve_list(request):
+	tienve_list = Tienve.objects.all()
+	return render(request, 'gcodedb/tienve_list.html', {'tienve_list':tienve_list})
+
+def tienve_form(request, id=None):
+    if request.method == "GET":
+        if id == None:
+            form = TienveForm()
+        else:
+            g2codes = Tienve.objects.get(pk = id)
+            form = TienveForm(instance=g2codes)
+        return render(request, "gcodedb/tienve_form.html", {'form': form})
+    else:
+        if id == None:
+            form = TienveForm(request.POST)
+        else:
+            g2codes = Tienve.objects.get(pk = id)
+            form = TienveForm(request.POST,instance= g2codes)
+        if form.is_valid():
+            form.save()
+        else:
+            print(form.add_error)
+        return redirect('/tienve/')
+
+def tienve_delete(request,id):
+    g2codes = Tienve.objects.filter(pk = id)
+    g2codes.delete()
+    return redirect('/tienve/')
+
+def danhgiansx_list(request):
+	danhgiansx_list = DanhgiaNSX.objects.all()
+	return render(request, 'gcodedb/danhgiansx_list.html', {'danhgiansx_list':danhgiansx_list})
+
+def danhgiansx_form(request, id=None):
+    if request.method == "GET":
+        if id == None:
+            form = DanhgiaNSXForm()
+        else:
+            g2codes = DanhgiaNSX.objects.get(pk = id)
+            form = DanhgiaNSXForm(instance=g2codes)
+        return render(request, "gcodedb/danhgiansx_form.html", {'form': form})
+    else:
+        if id == None:
+            form = DanhgiaNSXForm(request.POST)
+        else:
+            g2codes = DanhgiaNSX.objects.get(pk = id)
+            form = DanhgiaNSXForm(request.POST,instance= g2codes)
+        if form.is_valid():
+            g2codes = form.save(commit=False)
+            g2codes.dateupdate = date.today()
+            g2codes.save()
+            danhgiacodes = form.cleaned_data.get('danhgiacode')
+            for item in danhgiacodes:
+                g2codes.danhgiacode.add(item)
+        else:
+            print(form.add_error)
+        return redirect('/danhgiansx/')
+
+def danhgiansx_delete(request,id):
+    g2codes = DanhgiaNSX.objects.filter(pk = id)
+    g2codes.delete()
+    return redirect('/danhgiansx/')
+
+def danhgiacode_list(request):
+	danhgiacode_list = Danhgiacode.objects.all()
+	return render(request, 'gcodedb/danhgiacode_list.html', {'danhgiacode_list':danhgiacode_list})
+
+def danhgiacode_form(request, id=None):
+    if request.method == "GET":
+        if id == None:
+            form = DanhgiacodeForm()
+        else:
+            g2codes = Danhgiacode.objects.get(pk = id)
+            form = DanhgiacodeForm(instance=g2codes)
+        return render(request, "gcodedb/danhgiacode_form.html", {'form': form})
+    else:
+        if id == None:
+            form = DanhgiacodeForm(request.POST)
+        else:
+            g2codes = Danhgiacode.objects.get(pk = id)
+            form = DanhgiacodeForm(request.POST,instance= g2codes)
+        if form.is_valid():
+            form.save()
+        else:
+            print(form.add_error)
+        return redirect('/danhgiacode/')
+
+def danhgiacode_delete(request,id):
+    g2codes = Danhgiacode.objects.filter(pk = id)
+    g2codes.delete()
+    return redirect('/danhgiacode/')
+
+def phat_list(request):
+	phat_list = Phat.objects.all()
+	return render(request, 'gcodedb/phat_list.html', {'phat_list':phat_list})
+
+def phat_form(request, id=None):
+    if request.method == "GET":
+        if id == None:
+            form = PhatForm()
+        else:
+            g2codes = Phat.objects.get(pk = id)
+            form = PhatForm(instance=g2codes)
+        return render(request, "gcodedb/phat_form.html", {'form': form})
+    else:
+        if id == None:
+            form = PhatForm(request.POST)
+        else:
+            g2codes = Phat.objects.get(pk = id)
+            form = PhatForm(request.POST,instance= g2codes)
+        if form.is_valid():
+            g2codes = form.save(commit=False)
+            g2codes.dateupdate = date.today()
+            g2codes.save()
+        else:
+            print(form.add_error)
+        return redirect('/phat/')
+
+def phat_delete(request,id):
+    g2codes = Phat.objects.filter(pk = id)
+    g2codes.delete()
+    return redirect('/phat/')
+
